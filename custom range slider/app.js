295af28a236a9a0bd993165553ec8c56b7f8d1be
erdemir123 <sadikerdemir93@gmail.com>
2022-08-28 22:57:43 +0300

    custom range slider created

---
+++ custom range slider/app.js
@@ -0,0 +1,23 @@
+const range = document.getElementById("range")
+const label = document.getElementById("label")
+
+// range.addEventListener("input", (e)=>{
+//     const value = e.target.value
+//     label.innerHTML= value
+//     label.value= value
+//     if(label.value<=20){
+//         label.style.backgroundColor="#ede15b"
+//     }
+//     else if(label.value<=40){
+//         label.style.backgroundColor="#edbf33"
+//     }
+//     else if(label.value<=60){
+//         label.style.backgroundColor="#ef9b20"
+//     }
+//     else if(label.value<=80){
+//         label.style.backgroundColor="#f46a9b"
+//     }
+//     else if(label.value<=100){
+//         label.style.backgroundColor="#ea5545"
+//     }
+// })
